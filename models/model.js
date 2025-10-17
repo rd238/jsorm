01301f20f8607096b105ac03a1f512949b0443c1
Башков Михаил Антонович <141922582+rd238@users.noreply.github.com>
2025-10-17 15:58:06 +0300

    код приведен в порядок

--- models/model.js
+++ models/model.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
+const {execSync} = require('child_process');
 const path = require('path');
-const { SettingsDataBase } = require('../settings/settings_database');
+const {SettingsDataBase} = require('../settings/settings_database');
 
-function field({
+function field(
+    {
         name = "name",
         type = "str",
         _null = true,
         unique = false,
         primary_key = false,
         foreign_key = "",
-    }){
+    }
+) {
 
     let is_null = _null && !primary_key ? "NULL" : primary_key ? "NOT NULL" : "NOT NULL";
     let _unique = unique ? "UNIQUE" : "";
@@ -40,39 +42,49 @@ class Model {
     }
 
     #create_model(...args) {
-        let request = `CREATE TABLE IF NOT EXISTS ${this.#name_table} (${args.join(',')});`;
+        let request = `CREATE TABLE IF NOT EXISTS ${this.#name_table} ` +
+            `(${args.join(',')});`;
 
         return execSync(this.#command + `"${request}"`, {encoding: 'utf8'});
     }
 
-    #where_to_get(obj) {
-        let keys = Object.keys(obj);
+    #where_to_get(args) {
+        let keys = Object.keys(args);
 
         return keys.map((val) => {
-            return `${val} = ` + (
-                typeof obj[val] === 'string'
-                    ?
-                    `'${obj[val]}'` : `${obj[val]}`)
-        }).join(' AND ');
+                return `${val} = ` + (
+                    typeof args[val] === 'string'
+                        ?
+                        `'${args[val]}'`
+                        :
+                        `${args[val]}`
+                )
+            }
+        ).join(' AND ');
     }
 
-    get(args, name_table="") {
+    get(args, name_table = "") {
         let where = this.#where_to_get(args);
 
-        let request = `SELECT * FROM ${name_table ? name_table : this.#name_table} WHERE ${where};`;
+        let request = `SELECT * ` +
+            `FROM ${name_table ? name_table : this.#name_table} ` +
+            `WHERE ${where};`;
 
-        return this.parse_table_to_objects(execSync(this.#command + `"${request}"`, {encoding: 'utf8'}));
+        return this.parse_table_to_objects(
+            execSync(this.#command + `"${request}"`, {encoding: 'utf8'})
+        );
     }
 
-    create(obj) {
-        let keys = Object.keys(obj);
+    create(args) {
+        let keys = Object.keys(args);
 
         let request = `INSERT INTO ${this.#name_table} (${keys.join(',')}) ` +
             `VALUES (${keys.map(
-            key => 
-                    typeof obj[key] == 'string' 
-                            ?
-                            `'${obj[key]}'` : `${obj[key]}`).join(',')});`;
+                key =>
+                    typeof args[key] == 'string'
+                        ?
+                        `'${args[key]}'` : `${args[key]}`
+            ).join(',')});`;
 
         return execSync(this.#command + `"${request}"`, {encoding: 'utf8'});
     }
@@ -83,19 +95,19 @@ class Model {
             let keys = Object.keys(obj);
             request += `INSERT INTO ${this.#name_table} (${keys.join(',')}) ` +
                 `VALUES (${keys.map(
-                    key => 
-                        typeof obj[key] == 'string' 
+                    key =>
+                        typeof obj[key] == 'string'
                             ?
                             `'${obj[key]}'` : `${obj[key]}`).join(',')});`;
         }
 
-        return execSync(this.#command  + `"${request}"`, {encoding: 'utf8'});
+        return execSync(this.#command + `"${request}"`, {encoding: 'utf8'});
     }
 
     get_or_create(args) {
         try {
             return this.parse_table_to_objects(this.get(args));
-        } catch(err) {
+        } catch (err) {
             this.create(args);
 
             return this.parse_table_to_objects(this.get(args));
@@ -103,7 +115,8 @@ class Model {
     }
 
     delete(args) {
-        let request = `DELETE FROM ${this.#name_table}`;
+        let request = `DELETE ` +
+            `FROM ${this.#name_table}`;
         if (args) {
             let where = this.#where_to_get(args);
             request += ` WHERE ${where};`;
@@ -112,10 +125,11 @@ class Model {
         return execSync(this.#command + `"${request}"`, {encoding: 'utf8'});
     }
 
-    filter(obj, name_table="") {
-        let request = `SELECT * FROM ${name_table ? name_table : this.#name_table}`;
-        if (obj) {
-            let where = this.#where_to_get(obj);
+    filter(args, name_table = "") {
+        let request = `SELECT * ` +
+            `FROM ${name_table ? name_table : this.#name_table}`;
+        if (args) {
+            let where = this.#where_to_get(args);
 
             request += ` WHERE ${where};`;
         }
@@ -123,12 +137,12 @@ class Model {
         return this.parse_table_to_objects(execSync(this.#command + `"${request}"`, {encoding: 'utf8'}));
     }
 
-    parse_table_to_objects(str) {
-        if (typeof str !== 'string') {
-            return str;
+    parse_table_to_objects(table) {
+        if (typeof table !== 'string') {
+            return table;
         }
 
-        let res = str.split('\r\n');
+        let res = table.split('\r\n');
 
         res.splice(1, 1);
         res.splice(-3, 3);
@@ -136,18 +150,22 @@ class Model {
         if (res.length === 0) {
             return [];
         }
+
         let objects = {name_table: this.#name_table, result: []};
         let columns = res[0].split(" | ").map(val => val.trim());
+
         for (let i = 1; i < res.length; i++) {
             let obj = {};
             let vals = res[i].split(" | ").map(val => val.trim());
+
             for (let j = 0; j < columns.length; j++) {
                 obj[columns[j]] = vals[j];
             }
+
             objects.result.push(obj);
         }
 
-        return objects
+        return objects;
     }
 
     upgrade_get(args) {
@@ -158,7 +176,10 @@ class Model {
                 value =>
                     value.includes("id_")
                         ?
-                        res[i][value] = this.get({id: res[i][value]}, value.substring(3)) : value);
+                        res[i][value] = this.get({id: res[i][value]}, value.substring(3))
+                        :
+                        value
+            );
         }
 
         return res;
@@ -172,19 +193,23 @@ class Model {
                 value =>
                     value.includes("id_")
                         ?
-                        res[i][value] = this.filter({id: res[i][value]}, value.substring(3)) : value);
+                        res[i][value] = this.filter({id: res[i][value]}, value.substring(3))
+                        :
+                        value
+            );
         }
 
         return res;
     }
 
-    select(fields, name_table= "", as = "") {
-        this.#compound_request = `SELECT ${fields ? fields.join(', ') : "*"} FROM ${name_table ? name_table : this.#name_table} `;
+    select(fields, name_table = "", as = "") {
+        this.#compound_request = `SELECT ${fields ? fields.join(', ') : "*"} ` +
+            `FROM ${name_table ? name_table : this.#name_table} `;
 
         return this;
     }
 
-    where(ysl1, operation,ysl2) {
+    where(ysl1, operation, ysl2) {
         this.#compound_request += `WHERE ${ysl1} ${operation} ${ysl2} `;
 
         return this;
@@ -205,7 +230,12 @@ class Model {
     end_request() {
         this.#compound_request += ";";
 
-        let result = this.parse_table_to_objects(execSync(this.#command + `"${this.#compound_request}"`, {encoding: 'utf8'}));
+        let result = this.parse_table_to_objects(
+            execSync(
+                this.#command + `"${this.#compound_request}"`,
+                {encoding: 'utf8'}
+            )
+        );
 
         this.#compound_request = "";
 
